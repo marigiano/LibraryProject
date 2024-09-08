bc7ba128dc460af9b4593acbd9607a94898c31de
marigiano <maria.ananieva1@gmail.com>
2024-09-08 17:10:29 +0200

    Change of project type

--- MyLibrary/obj/Debug/net8.0-windows/MyLibrary.AssemblyInfo.cs
+++ MyLibrary/obj/Debug/net8.0-windows/MyLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e7aa7719198432361dc25e3e2142bdd1ec15fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
