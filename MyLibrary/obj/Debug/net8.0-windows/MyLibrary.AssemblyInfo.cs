c5a8bfc4661aa40ac08ae7d8014ea432f10a9192
marigiano <maria.ananieva1@gmail.com>
2024-09-15 23:13:18 +0200

    Created Books DB, connected it to the project, added app.config and connectionString, installed packages SQLite.Core and Dapper

--- MyLibrary/obj/Debug/net8.0-windows/MyLibrary.AssemblyInfo.cs
+++ MyLibrary/obj/Debug/net8.0-windows/MyLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e7aa7719198432361dc25e3e2142bdd1ec15fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a4b05324b9c7d4564a071fb90327e18e6d1196")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
